157ceb170c1f5d734aea90f8463339584df7783e
Justine Dang <justsogaming253@gmail.com>
2018-07-28 19:33:06 -0700

    delete js

--- js/launchpage.js
+++ js/launchpage.js
@@ -1,20 +1,2 @@
-let x = document.getElementById("playBtn");
-if(x){
-  x.addEventListener('click', myFunction);
-}
-else{
-  setTimeout(myFunction, 1000)
-}
-// let x = document.getElementById("playBtn")
-// x.addEventListener("click", myFunction);
 
-function myFunction(){
-  x = document.getElementById("playBtn");
-  x.addEventListener('click', pressBtn);
-}
 
-function pressBtn(){
-  let nick = prompt("Enter a nickname");
-  console.log(nick);
-  
-}
