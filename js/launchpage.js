46b5ecd4d203e60136f9f87f15358596174ad4fc
lillee205 <leelillian205@gmail.com>
2018-07-28 17:18:58 -0700

    fade in

---
+++ js/launchpage.js
@@ -0,0 +1,19 @@
+let x = document.getElementById("playBtn");
+if(x){
+  x.addEventListener('click', myFunction);
+}
+else{
+  setTimeout(myFunction, 2000)
+}
+// let x = document.getElementById("playBtn")
+// x.addEventListener("click", myFunction);
+
+function myFunction(){
+  x = document.getElementById("playBtn");
+  x.addEventListener('click', pressBtn);
+}
+
+function pressBtn(){
+  let nick = prompt("Enter a nickname");
+  console.log(nick);
+}
